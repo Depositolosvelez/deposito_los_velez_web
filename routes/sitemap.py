e64b7081019815b85f1ae341e9e86581f624bf94
Depositolosvelez <depositolosvelez@gmail.com>
2026-05-26 15:22:35 -0500

    SEO: actualizar URLs a velezdepositos.com.co

--- routes/sitemap.py
+++ routes/sitemap.py
@@ -2,7 +2,6 @@ from flask import Blueprint, Response
 
 sitemap_bp = Blueprint('sitemap', __name__)
 
-
 @sitemap_bp.route('/robots.txt')
 def robots():
     content = """User-agent: *
@@ -12,8 +11,7 @@ Allow: /contacto
 Disallow: /admin
 Disallow: /admin/
 Disallow: /static/
-
-Sitemap: https://depositolosvelez.pythonanywhere.com/sitemap.xml"""
+Sitemap: https://velezdepositos.com.co/sitemap.xml"""
     return Response(content, mimetype='text/plain')
 
 @sitemap_bp.route('/sitemap.xml')
@@ -21,15 +19,15 @@ def sitemap():
     xml = '''<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://depositolosvelez.pythonanywhere.com/</loc>
+        <loc>https://velezdepositos.com.co/</loc>
         <priority>1.0</priority>
     </url>
     <url>
-        <loc>https://depositolosvelez.pythonanywhere.com/productos</loc>
+        <loc>https://velezdepositos.com.co/productos</loc>
         <priority>0.9</priority>
     </url>
     <url>
-        <loc>https://depositolosvelez.pythonanywhere.com/contacto</loc>
+        <loc>https://velezdepositos.com.co/contacto</loc>
         <priority>0.8</priority>
     </url>
 </urlset>'''
